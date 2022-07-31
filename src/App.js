466e09ee86bed462a022cf3a31c0b196467073e7
sabrina <sabrinaabrodi20@gmail.com>
2022-07-31 20:02:02 +0200

    adding fuctionality!!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
-import React from "react";
+// import { useState } from "react";
+
+// function App() {
+//   const [isActive, setIsActive] = useState(false);
+
+//   const handleClick = (div) => {
+//     setIsActive((current) => !current);
+//   };
+
+//   return (
+//     <div className="container">
+//       <div
+//         className={`panel p1 active ${isActive ? "active" : ""} `}
+//         onClick={handleClick}
+//       ></div>
+//       <div
+//         className={`panel p2 ${isActive ? "active" : ""} `}
+//         onClick={handleClick}
+//       ></div>
+//       <div
+//         className={`panel p3 ${isActive ? "active" : ""} `}
+//         onClick={handleClick}
+//       ></div>
+//       <div
+//         className={`panel p4 ${isActive ? "active" : ""} `}
+//         onClick={handleClick}
+//       ></div>
+//       <div
+//         className={`panel p5 ${isActive ? "active" : ""} `}
+//         onClick={handleClick}
+//       ></div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { useState } from "react";
+import "./index.css";
 
 function App() {
+  const [appState, changeState] = useState({
+    activeAbject: null,
+    objects: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }],
+  });
+
+  function toggleActive(index) {
+    changeState({ ...appState, activeObject: appState.objects[index] });
+  }
+
+  function toggleActiveStyles(index) {
+    if (appState.objects[index] === appState.activeObject) {
+      return "panel active";
+    } else {
+      return "panel inactive";
+    }
+  }
+
   return (
     <div className="container">
-      <div className="panel p1 active"></div>
-      <div className="panel p2"></div>
-      <div className="panel p3"></div>
-      <div className="panel p4"></div>
-      <div className="panel p5"></div>
+      {appState.objects.map((elemnts, index) => (
+        <div
+          key={index}
+          className={toggleActiveStyles(index)}
+          onClick={() => {
+            toggleActive(index);
+          }}
+        ></div>
+      ))}
     </div>
   );
 }
